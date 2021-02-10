767c10e49cd0d5d5691c8209fbf766abada1d108
TAJ <bestrapboy@gmail.com>
2021-02-10 21:01:10 +0100

    add basic shit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.png';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 150,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [age, setAge] = React.useState('');
+	const classes = useStyles();
+	const handleChange = (event) => {
+		setAge(event.target.value);
+	};
+
+	return (
+		<div className="App">
+			<header className="header">
+				<img src={logo} className="logo" alt="logo" />
+				<p>Családi lottó</p>
+			</header>
+			<div className="content">
+				<FormControl variant="outlined" className={classes.formControl}>
+					<InputLabel id="name-label">Neved</InputLabel>
+					<Select
+						labelId="name-label"
+						id="name"
+						value={age}
+						onChange={handleChange}
+						label="Neved"
+					>
+						<MenuItem value="">
+							<em>válassz!</em>
+						</MenuItem>
+						<MenuItem value={"andi"}>Andi</MenuItem>
+						<MenuItem value={"atis"}>Atis</MenuItem>
+						<MenuItem value={"benji"}>Benji</MenuItem>
+						<MenuItem value={"bia"}>Bia</MenuItem>
+						<MenuItem value={"doma"}>Doma</MenuItem>
+						<MenuItem value={"edit"}>Edit</MenuItem>
+						<MenuItem value={"taj"}>TAJ</MenuItem>
+						<MenuItem value={"tika"}>Tika</MenuItem>
+						<MenuItem value={"timi"}>Timi</MenuItem>
+						<MenuItem value={"zoli"}>Zoli</MenuItem>
+					</Select>
+				</FormControl>
+				<div className={classes.root}>
+					<Button variant="contained" color="primary">
+						Regisztrálok
+					</Button>
+				</div>
+			</div>
+			<footer className="footer">
+				<p>Powered by TAJ 2021</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
